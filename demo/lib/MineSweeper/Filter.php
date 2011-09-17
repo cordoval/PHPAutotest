139f3add0a34dcaa206ac6018f8c23429773c5ee
Luis Cordova <cordoval@gmail.com>
2011-09-17 16:47:57 -0500

    refactored in filter the closure for stencilMask

--- demo/lib/MineSweeper/Filter.php
+++ demo/lib/MineSweeper/Filter.php
@@ -5,6 +5,10 @@ class Filter implements FilterInterface
 {
     protected $stenciler;
 
+    protected $x;
+    protected $y;
+    protected $grid;
+
     public function __construct(StencilerInterface $stenciler) {
         $this->stenciler = $stenciler;
     }
@@ -19,22 +23,31 @@ class Filter implements FilterInterface
      * @return value
      */
     public function process($grid, $x, $y) {
-
-        // gets the numeral on each neighbor coordinate
-        $stencilMasker = function ($c) use ($grid, $x, $y)
-        {
-            $xPointer = $x + $c[0];
-            $yPointer = $y + $c[1];
-            $xLimit = sizeof($grid[0]);
-            $yLimit = sizeof(array_keys($grid));
-            $out = (($xPointer < 0) || ($yPointer < 0) ||
-                ($xPointer >= $xLimit) ||
-                ($yPointer >= $yLimit)) ? '.' : $grid[$yPointer][$xPointer];
-            return $out;
-        };
+        $this->x = $x;
+        $this->y = $y;
+        $this->grid = $grid;
 
         // map-processes and returns the array of neighbors numerals
-        return array_map($stencilMasker, $this->stenciler->getStencil());
+        return array_map(array($this, 'stencilMask'), $this->stenciler->getStencil());
+    }
+
+    /**
+     * gets the numeral on each neighbor coordinate
+     *
+     * @param int $c
+     *
+     * @return value
+     */
+    public function stencilMask(int $c)
+    {
+        $xPointer = $this->x + $c[0];
+        $yPointer = $this->y + $c[1];
+        $xLimit = sizeof($this->grid[0]);
+        $yLimit = sizeof(array_keys($this->grid));
+        $out = (($xPointer < 0) || ($yPointer < 0) ||
+            ($xPointer >= $xLimit) ||
+            ($yPointer >= $yLimit)) ? '.' : $this->grid[$yPointer][$xPointer];
+        return $out;
    }
 }
 
